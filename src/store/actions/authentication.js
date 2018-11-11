460d523842e6e78326109feeeb8c945a44ef9e2c
Khalid Mohamed <kalmohammed56@gmail.com>
2018-11-11 10:46:28 +0300

    Authentication Methods in Actions

--- src/store/actions/authentication.js
+++ src/store/actions/authentication.js
@@ -9,15 +9,81 @@ const instance = axios.create({
   baseURL: "https://api-chatr.herokuapp.com/"
 });
 
-const setAuthToken = token => {};
+const setAuthToken = token => {
+  console.log(token);
+  localStorage.setItem("token", token);
+  axios.defaults.headers.common.Authorization = `jwt ${token}`;};
 
-export const checkForExpiredToken = () => {};
+export const checkForExpiredToken = () => {
+  return dispatch => {
+    // Get token
+    const token = localStorage.token;
 
-export const login = userData => {};
+    if (token) {
+      const currentTime = Date.now() / 1000;
 
-export const signup = userData => {};
+      // Decode token and get user info
+      const user = jwt_decode(token);
 
-export const logout = () => {};
+      // Check token expiration
+      if (user.exp >= currentTime) {
+        // Set auth token header
+        setAuthToken(token);
+        // Set user
+        dispatch(setCurrentUser(user));
+      } else {
+        dispatch(logout());
+      }
+    }
+  };
+};
+
+export const login = (userData, history) => {
+  return dispatch => {
+    axios
+      .post("https://the-index-api.herokuapp.com/login/", userData)
+      .then(res => res.data)
+      .then(user => {
+        const decodedUser = jwt_decode(user.token);
+        setAuthToken(user.token);
+        dispatch(setCurrentUser(decodedUser));
+        history.goBack();
+      })
+      .catch(err => console.error(err.response));
+  };
+};
+
+export const signup = (userData, history) => {
+  return dispatch => {
+    axios
+      .post("https://the-index-api.herokuapp.com/signup/", userData)
+      .then(res => res.data)
+      .then(user => {
+        const decodedUser = jwt_decode(user.token);
+        setAuthToken(user.token);
+        dispatch(setCurrentUser(decodedUser));
+        history.goBack();
+      })
+      .catch(err => console.error(err.response));
+  };
+};
+
+export const signup = (userData, history) => {
+  return dispatch => {
+    axios
+      .post("https://the-index-api.herokuapp.com/signup/", userData)
+      .then(res => res.data)
+      .then(user => {
+        const decodedUser = jwt_decode(user.token);
+        setAuthToken(user.token);
+        dispatch(setCurrentUser(decodedUser));
+        history.goBack();
+      })
+      .catch(err => console.error(err.response));
+  };
+};
+
+export const logout = () => setCurrentUser();
 
 const setCurrentUser = user => ({
   type: actionTypes.SET_CURRENT_USER,
