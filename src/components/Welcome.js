ec645dcf4225fde8f15b67243b330d13d34ad0b7
Khalid Mohamed <kalmohammed56@gmail.com>
2018-11-18 08:39:54 +0300

    Fixed Welcome page design

--- src/components/Welcome.js
+++ src/components/Welcome.js
@@ -3,19 +3,31 @@ import { Link, Redirect } from "react-router-dom";
 import { connect } from "react-redux";
 
 class Welcome extends Component {
-
   render() {
-    let message=()=>(this.props.user? `Welcome ${this.props.user.username}` : "You're gonna need to login to see the messages")
+    let message = () =>
+      this.props.user
+        ? `Welcome ${this.props.user.username}`
+        : "You're gonna need to login to see the messages";
     return (
       <header className="masthead d-flex">
         <div className="container text-center my-auto z-1">
           <h1 className="mb-1">WELCOME TO CHATR</h1>
           <h3 className="mb-5">
-    <em>{this.props.user? `${this.props.user.username}` : <div>"You're gonna need to login to see the messages"<div><Link to="/login" className="btn btn-primary btn-lg">
-            Login
-          </Link></div></div>}</em>
+            <h1 className="h1">
+              {this.props.user ? (
+                `${this.props.user.username.toUpperCase()}`
+              ) : (
+                <div>
+                  "You're gonna need to login to see the messages"
+                  <div>
+                    <Link to="/login" className="btn btn-primary btn-lg">
+                      Login
+                    </Link>
+                  </div>
+                </div>
+              )}
+            </h1>
           </h3>
-          
         </div>
         <div className="overlay z-0" />
       </header>
@@ -24,9 +36,8 @@ class Welcome extends Component {
 }
 const mapStateToProps = state => {
   return {
-    user: state.rootAuth.user,
+    user: state.rootAuth.user
   };
 };
 
 export default connect(mapStateToProps)(Welcome);
-
